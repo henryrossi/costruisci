2d8af8f26278b2d64d87a60bf3759f401b4685b2
Henry Rossi <henry.rossi219@gmail.com>
2024-11-30 12:00:50 -0800

    first commit

---
+++ costruisci.h
@@ -0,0 +1,12 @@
+/* -------------------------------------------------------------------------- */
+//  Goals For This Project
+//
+//  [ ] compile source files
+//
+
+#ifndef BUILD_H
+#define BUILD_H
+
+#define 
+
+#endif
